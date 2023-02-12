0098db873c749931fa2a27ff06996812e8ca6bec
mateuszmistecki <mateusz.mistecki@gmail.com>
2023-02-12 12:40:38 +0100

    Get info from Catfact api using axios

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,13 +5,36 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+    <q-btn @click="changeValue">
+      Change value
+    </q-btn>
+    <div>
+      {{lorem}}
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { ref } from 'vue'
+import axios from "axios";
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup() {
+    const lorem = ref("Lorem bla bla")
+
+    function changeValue() {
+      // lorem.value = lorem.value + "1";
+      axios.get("https://catfact.ninja/fact")
+        .then((response) => {lorem.value = response.data.fact})
+    }
+
+    // expose to template and other options API hooks
+    return {
+      lorem,
+      changeValue
+    }
+  }
 })
 </script>
